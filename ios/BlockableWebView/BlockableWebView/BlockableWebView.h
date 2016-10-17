d387717781eeba76b63cf3ff4faf8b3950c6f898
Rafael Seemann <seemann@rafaels-mbp-2.localdomain>
2016-10-17 23:51:56 +0200

    Add initial iOS project structure

---
+++ ios/BlockableWebView/BlockableWebView/BlockableWebView.h
@@ -0,0 +1,13 @@
+//
+//  BlockableWebView.h
+//  BlockableWebView
+//
+//  Created by Rafael Seemann on 17/10/16.
+//  Copyright © 2016 Rafael Seemann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockableWebView : NSObject
+
+@end
---
+++ ios/BlockableWebView/BlockableWebView/BlockableWebView.m
@@ -0,0 +1,13 @@
+//
+//  BlockableWebView.m
+//  BlockableWebView
+//
+//  Created by Rafael Seemann on 17/10/16.
+//  Copyright © 2016 Rafael Seemann. All rights reserved.
+//
+
+#import "BlockableWebView.h"
+
+@implementation BlockableWebView
+
+@end
