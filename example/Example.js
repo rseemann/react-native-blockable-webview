9c016899b540e111981db4c246469f491b30b730
Rafael Seemann <seemann@rafaels-mbp-2.localdomain>
2016-10-22 23:06:42 +0200

    Block navigation to issues url

--- example/Example.js
+++ example/Example.js
@@ -13,12 +13,26 @@ import {
 import BlockableWebView from 'react-native-blockable-webview';
 
 export default class example extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onNavigationBlocked = this.onNavigationBlocked.bind(this);
+  }
+
+  onNavigationBlocked(request) {
+    console.log(request);
+  }
+
   render() {
-    const source = {uri: "https://github.com/facebook/react-native"};
+    const source = {uri: 'https://github.com/facebook/react-native'};
+    const policy = [{url: 'https://github.com/facebook/react-native/issues'}];
 
     return (
       <View style={styles.container}>
-        <BlockableWebView style={styles.webview} source={source}/>
+        <BlockableWebView style={styles.webview} source={source}
+          navigationBlockingPolicies={policy}
+          onNavigationBlocked={this.onNavigationBlocked}
+        />
       </View>
     );
   }
