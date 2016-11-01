23353bce7250862252b59d2b3d660c517d51dbdf
Rafael Seemann <rseemann@gmail.com>
2016-11-02 00:37:16 +0100

    Add all Java files needed for the blockable webview

---
+++ example/android/app/src/main/java/com/example/BlockableWebView.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by seemann on 01/11/16.
+ */
+public class BlockableWebView implements ReactPackage {
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList(
+                new BlockableWebViewManager()
+        );
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+        return Collections.emptyList();
+    }
+}
---
+++ example/android/app/src/main/java/com/example/BlockableWebViewEvent.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.Event;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+
+/**
+ * Created by seemann on 01/11/16.
+ */
+
+public class BlockableWebViewEvent extends Event<BlockableWebViewEvent> {
+
+    public static final String EVENT_NAME = "navigationBlocked";
+    private WritableMap mEventData;
+
+    public BlockableWebViewEvent(int viewId, WritableMap eventData) {
+        super(viewId);
+        mEventData = eventData;
+    }
+
+
+    @Override
+    public String getEventName() {
+        return EVENT_NAME;
+    }
+
+    @Override
+    public boolean canCoalesce() {
+        return false;
+    }
+
+    @Override
+    public short getCoalescingKey() {
+        // All events for a given view can be coalesced.
+        return 0;
+    }
+
+    @Override
+    public void dispatch(RCTEventEmitter rctEventEmitter) {
+        rctEventEmitter.receiveEvent(getViewTag(), getEventName(), mEventData);
+    }
+}
